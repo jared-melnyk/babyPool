8e532be5bcba6dc7d23a4e3bd210950b172a72f6
Jared Melnyk <jaredmelnyk@Jareds-MacBook-Air.local>
2020-09-08 08:42:26 -0500

    first commit

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set("view engine", "ejs");
 
 const mongoose = require("mongoose");
 const { query } = require("express");
-const uri = process.env.MONGODB_URL;
+const uri = process.env.MONGODB_URL_BABYAPP;
 mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -23,175 +23,75 @@ conn.on('connected', () => {
     console.log("Mongoose is connected!");
 });
 
-let TeamSchema = new mongoose.Schema({
-    _id: Number,
-    name: String,
-    epScores: Array,
-    roster: Array
+let GuessSchema = new mongoose.Schema({
+    guesserName: String,
+    guesserEmail: String,
+    birthDate: Date,
+    babyLength: Number,
+    babyWeight: {
+      pounds: Number,
+      ounces: Number
+    },
+    babySex: String
 }, {minimize:false});
 
-let Team = mongoose.model("Team", TeamSchema);
-
-let BakerSchema = new mongoose.Schema({
-  name: String,
-  img: String,
-  bio: String,
-  epScores: [{
-    episode: Number,
-    scores: [{
-      category: String,
-      score: Number
-    }]
-  }]
-}, {minimize:false});
-
-let Baker = mongoose.model("Baker", BakerSchema);
-
-let EpisodeScoreSchema = new mongoose.Schema({
-    _id: String,
-    episode: Number,
-    bakerName: String,
-    scores: [{
-        _id: Number,
-        category: String,
-        score: Number
-    }]
-}, {minimize:false});
-
-let EpisodeScore = mongoose.model("EpisodeScore", EpisodeScoreSchema);
+let Guess = mongoose.model("Guess", GuessSchema);
 
 app.get("/", function(req, res) {
-    res.redirect("/teams");
+    res.redirect("/guesses");
 });
 
-app.get("/teamPage/:teamId", function(req, res) {
-    Team.find({_id: req.params.teamId}, function(err, team) {
-        if(err){
-            console.log(err);
-        } else {
-            Baker.find({}, function(err, allBakers) {
-                if(err){
-                    console.log(err);
-                } else {
-                    res.render("teamPage", {team: team, allBakers: allBakers});
-                }
-            });
-        }
-    });
+app.get("/submitGuess", function(req, res) {
+  res.render("submitGuess");
 });
 
-app.get("/input-scores", function(req, res) {
-  let submit = req.query.submit || false;  
-  Baker.find({}, function(err, allBakers) {
+app.get("/guesses", function(req, res) {
+  // Get all guesses from DB
+  Guess.find({}).sort({birthDate: 1}).exec(function(err, allGuesses) {
     if(err){
-      console.log(err);
+        console.log(err);
     } else {
-      EpisodeScore.find({episode:0}, function(err, score) {
-        if(err){
-          console.log(err);
-        } else {
-          res.render("inputScores", {submit: submit, allBakers: allBakers, score: score});
-        }
-      });
+        res.render("guesses", {allGuesses: allGuesses});
     }
   });
 });
 
-app.get("/teams", function(req, res) {
-    // Get all teams from DB
-    Team.find({}, function(err, allTeams) {
-        if(err){
-            console.log(err);
-        } else {
-            Baker.find({}, function(err, allBakers) {
-                if(err){
-                    console.log(err);
-                } else {
-                    
-                    res.render("teams", {allTeams: allTeams, allBakers: allBakers});
-                }
-            });
-        }
-    });
-});
-
-app.get("/bakers", function(req, res) {
-    Baker.find({}, function(err, allBakers) {
-        if(err){
-            console.log(err);
-        } else {
-            res.render("bakers", {allBakers: allBakers});
-        }
-    });
-});
-
-app.get("/rosters", function(req, res) {
-    Team.find({}, function(err, allTeams) {
-        if(err){
-            console.log(err);
-        } else {
-            Baker.find({}, function(err, allBakers){
-                if(err){
-                    console.log(err);
-                } else {
-                    res.render("rosters", {teamList: allTeams, allBakers: allBakers});
-                }
-            });
-        }
-    });
-});
-
-app.post("/input-scores", function(req, res) {
-  Baker.findOne({"name": req.body.baker}, function(err, baker) {
-    if(err) return res.status(500).send({error:err});
-    let bakerId = baker._id;
-    EpisodeScore.find({episode: 0}, function(err, epScore) {
-      if(err) return res.status(500).send({error:err});
-      let newEpScore = {
-        episode: req.body.episode,
-        scores: []
-      };
-      for(let [index, scoreCat] of epScore[0].scores.entries()) {
-        let catString = scoreCat.category;
-        newEpScore.scores.push({id: index, category: catString, score: req.body[catString]});
-      };
-      Baker.findOneAndUpdate(
-        {_id: bakerId}, 
-        { $addToSet: {epScores: newEpScore}}, 
-        {upsert: true},  
-        function(err, daScore){
-          if(err){
-            console.log(err);
-          } else {
-            res.redirect("/input-scores?submit=true");
-          }
-      });
-    });
+app.post("/guesses", function(req, res) {
+  Guess.countDocuments({guesserEmail: req.body.guesserEmail}, function(err, result) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(result);
+    }
+  });
+  
+  let d = new Date(req.body.birthDate);
+  let displayDate = d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate() + " " + d.getHours() + ":" + d.getMinutes();
+  console.log(displayDate);
+  let newGuess = {
+    guesserName: req.body.guesserName,
+    guesserEmail: req.body.guesserEmail,
+    birthDate: displayDate,
+    babyLength: req.body.babyLength,
+    babyWeight: {
+      pounds: req.body.babyWeightPounds,
+      ounces: req.body.babyWeightOunces
+    },
+    babySex: req.body.babySex
+  }
+  console.log(newGuess);
+  Guess.findOneAndUpdate(
+    {guesserEmail: req.body.guesserEmail}, 
+    {$set: newGuess}, 
+    {upsert: true},  
+    function(err, guess){
+      if(err){
+        console.log(err);
+      } else {
+        res.redirect("/guesses?newGuess=true");
+      }
   });
 });
-
-app.post("/teams", function(req, res) {
-    let newTeam = {
-        name: req.body.name,
-        slogan: req.body.slogan,
-        epScores:{},
-        bakers:{}
-    };
-
-    Team.create(newTeam, function(err, newlyCreated) {
-        if(err){
-            console.log(err);
-        } else {
-            res.redirect("/");
-        }
-    });
-    
-});
-
-app.get("/add-team", function(req, res) {
-    res.render("addTeam");
-});
-
 
 let port = process.env.PORT || 8080;
 app.listen(port);
